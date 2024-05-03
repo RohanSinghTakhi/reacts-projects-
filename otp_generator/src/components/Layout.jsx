5327c2f12e69e453b7d70163a0cfb75d1e80f7df
RohanSinghTakhi <co23545@ccet.ac.in>
2024-05-03 18:38:17 +0530

    layout complete

---
+++ otp_generator/src/components/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Layout() {
+  return (
+    <div>
+      <div className="box1">
+        <div className="number">
+          
+        </div>
+        <button>{/* Button content */}</button>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
