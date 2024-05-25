96aae298df92f540e06020684207da3068c783b4
RohanSinghTakhi <co23545@ccet.ac.in>
2024-05-25 14:21:07 +0530

    todo

--- otp_generator/src/components/Layout.jsx
+++ otp_generator/src/components/Layout.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 function Layout() {
   return (
     <div>
+      
       <div className="box1">
-        <div className="number">
-          
-        </div>
-        <button>{/* Button content */}</button>
+      <h2>OTP GENERATOR</h2>
+      <div className="number">
+        <div className="num">0</div>
+        <div className="num">0</div>
+        <div className="num">0</div>
+        <div className="num">0</div>
+      </div>
+        <button className='button' type="button">Generate</button>
       </div>
     </div>
   );
