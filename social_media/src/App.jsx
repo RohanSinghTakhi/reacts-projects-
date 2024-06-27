7373563d0bf0e82b20d8e7e4710d8d77abae1c4f
RohanSinghTakhi <co23545@ccet.ac.in>
2024-06-27 12:03:41 +0530

    post creation

--- social_media/src/App.jsx
+++ social_media/src/App.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 import './App.css';  // Make sure to import the CSS file
 import Nav from './components/Nav';
 import Footer from './components/Footer';
+import SideBar from './components/SideBar';
+import Forms from './components/forms';
 
 function App() {
   return (
-  <>
-  <Nav></Nav>
-  <Footer></Footer>
-  </>
-  )
+    <>
+      <Nav />
+      <div className="main-content">
+        <div className="sidebar">
+          <SideBar />
+        </div>
+        <div className="content">
+          <Forms />
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
