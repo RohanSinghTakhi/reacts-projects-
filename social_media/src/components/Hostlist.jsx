4c2b01ce7defe137e8523352d0f07035a97f21e2
RohanSinghTakhi <co23545@ccet.ac.in>
2024-07-02 17:41:31 +0530

    done

--- social_media/src/components/Hostlist.jsx
+++ social_media/src/components/Hostlist.jsx
@@ -1,13 +1,16 @@
+import { useContext } from "react";
 import Post from "./Post";
+import { PostList } from "../components/post-list-store";
 
-const PostList = ()=>{
-    return(
-        <>
-        <Post></Post>
-        <Post></Post>
-        <Post></Post>
-        </>
-    )
-}
+const PostList1 = () => {
+  const { postList } = useContext(PostList);
+  return (
+    <>
+      {postList.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </>
+  );
+};
 
-export default PostList
+export default PostList1;
--- social_media/src/components/Post.jsx
+++ social_media/src/components/Post.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { PostList } from '../components/post-list-store';
 
-function Post() {
+function Post({ post }) {
+  const { deletePost } = useContext(PostList);
+
   return (
     <div className="card" style={{ width: '18rem' }}>
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
+        <h5 className="card-title">{post.title}</h5>
         <p className="card-text">
-          Some quick example text to build on the card title and make up the bulk of the card's content.
+          {post.body}
         </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
+        <div className="likes" style={{ fontSize: "14px", fontWeight: "bolder", margin: "4px 2px" }}>
+          Likes: {post.reactions}
+        </div>
+        <a href="#" className="btn btn-danger" onClick={() => deletePost(post.id)}>
+          Delete
         </a>
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+          {post.tags.map((tag, index) => (
+            <p key={index} style={{ fontSize: "15px", color: "grey", margin: "4px 2px" }}>
+              #{tag}
+            </p>
+          ))}
+        </div>
       </div>
     </div>
   );
