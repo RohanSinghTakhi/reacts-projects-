870c0eea5d04872ef2f0421a34cfad8fcd698ce0
RohanSinghTakhi <co23545@ccet.ac.in>
2024-07-03 16:45:00 +0530

    login done

--- login_form/src/App.jsx
+++ login_form/src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
+import { useState ,useContext} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from './components/Form';
+import LoginIds from './store/LoginIds'
+import "./App.css"
+import Display from './components/Displays';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-     
-    </>
+    <LoginIds>
+     <Form></Form>
+     <Display></Display>
+    </LoginIds> 
   )
 }
 
---
+++ login_form/src/components/Displays.jsx
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react';
+import UserContext from '../store/UserContext';
+
+function Display() {
+  const { user } = useContext(UserContext);
+
+  return (
+    <div>{user}</div>
+  );
+}
+
+export default Display;
